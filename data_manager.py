6f3916d8fba5fb3b5f82588a1320e8aeb7ab1959
gyurma12 <gyurma1292@gmail.com>
2021-02-03 09:20:23 +0100

    half done database

--- data_manager.py
+++ data_manager.py
@@ -24,10 +24,11 @@ def get_answers(cursor: RealDictCursor) -> list:
     cursor.execute(query)
     return cursor.fetchall()
 
+
 @database_common.connection_handler
 def get_data_question(cursor: RealDictCursor, id: int) -> list:
     query = """
-        SELECT id, submission_time, vote_number, question_id, message, image
+        SELECT id, submission_time, view_number, vote_number, title, message, img
         FROM question
         WHERE id=%(id)s
         ORDER BY submission_time"""
@@ -46,26 +47,57 @@ def get_data_answer(cursor: RealDictCursor, id: int) -> list:
 
 
 @database_common.connection_handler
-def add_new_comment(cursor: RealDictCursor,id:int,question_id:int,answer_id:int, message: str,submission_time:int, edited_number:int) -> list:
+def add_new_comment(cursor: RealDictCursor, question_id: int, answer_id: int, message: str, submission_time: int, edited_number: int) -> list:
     query = """
         INSERT INTO comment(id, question_id, answer_id, message, submission_time, edited_number)
-        VALUE %(id)s, %(question_id)s, %(answer_id)s, %(message)s, %(submission_time)s, %(edited_number)s
+        VALUES (%(id)s, %(question_id)s, %(answer_id)s, %(message)s, %(submission_time)s, %(edited_number)s)
         """
-    cursor.execute(query, {"id": id,"question_id":question_id, "answer_id":answer_id, "message":message, "submission_time":submission_time, "edited_number":edited_number})
-    return cursor.fetchall()
+    cursor.execute(query, {"id": id, "question_id": question_id, "answer_id": answer_id, "message": message, "submission_time": submission_time, "edited_number": edited_number})
 
 
-'''@database_common.connection_handler
-def update_applicant_phone(cursor: RealDictCursor, number:str, code:int) -> list:
+
+@database_common.connection_handler
+def add_new_answer(cursor: RealDictCursor,submission_time: int, vote_number: int, question_id: int, message: str,  image:str) -> list:
     query = """
-        UPDATE applicant
-        SET phone_number = %(number)s
-        WHERE application_code = %(code)s"""
-    cursor.execute(query,{"number":number, "code":code})'''
+        INSERT INTO answer(submission_time,vote_number, question_id, message, image)
+        VALUES (%(submission_time)s, %(vote_number)s, %(question_id)s, %(message)s,  %(image)s)
+        """
+    cursor.execute(query, {"question_id": question_id, "message": message, "submission_time": submission_time,"vote_number":vote_number, "image": image})
 
 
 
+@database_common.connection_handler
+def add_new_question(cursor: RealDictCursor, submission_time: int, view_number: int, vote_number: int, title: str, message: str, img: str) -> list:
+    query = """
+        INSERT INTO question( submission_time, view_number, vote_number, title, message, img)
+        VALUES  (%(submission_time)s, %(view_number)s, %(vote_number)s, %(title)s, %(message)s, %(img)s)
+        """
+    cursor.execute(query, {"submission_time":submission_time, "view_number":view_number, "vote_number":vote_number, "title":title, "message":message, "img":img})
 
 
+@database_common.connection_handler
+def update_question(cursor: RealDictCursor, title:str, message: str, id:int) -> list:
+    query = """
+        UPDATE question
+        SET title = %(title)s, message = %(message)s
+        WHERE id = %(id)s"""
+    cursor.execute(query,{"title":title, "message":message, "id":id})
 
+
+@database_common.connection_handler
+def update_question_vote(cursor: RealDictCursor, vote_number:int, id:int) -> list:
+    query = """
+        UPDATE question
+        SET vote_number = %(vote)s
+        WHERE id = %(id)s"""
+    cursor.execute(query,{"vote":vote_number, "id":id})
+
+
+@database_common.connection_handler
+def update_answer_vote(cursor: RealDictCursor, vote_number:int, id:int) -> list:
+    query = """
+        UPDATE answer
+        SET vote_number = %(vote)s
+        WHERE id = %(id)s"""
+    cursor.execute(query,{"vote":vote_number, "id":id})
 
--- server.py
+++ server.py
@@ -6,31 +6,6 @@ import csv
 from datetime import datetime
 
 app = Flask(__name__)
-def get_data(file, id):
-    for row in file:
-        if row['id'] == id:
-            return row
-
-def get_data_answer(file, id):
-    for row in file:
-        if row['question_id'] == id:
-            return row
-
-
-def get_id(questions):
-    sorted_questions = sorted(questions, key=itemgetter("id"))
-    if len(sorted_questions)>0:
-        return int(sorted_questions[-1]['id'])+1
-    else:
-        return 1    
-
-def get_id_ans(answers):
-    sorted_answers = sorted(answers, key=itemgetter("id"))
-    if len(sorted_answers)>0:
-        return int(sorted_answers[-1]['id'])+1
-    else:
-        return 1    
-
 
 @app.route("/")
 @app.route("/list")
@@ -42,51 +17,28 @@ def list():
 
 @app.route("/question/<string:question_id>/", methods=["POST", "GET"])
 def display_question(question_id):
-    questions = data_manager.get_questions()
+    questions = data_manager.get_data_question(question_id)
     answers = data_manager.get_answers()
     return render_template("question.html", question_id=question_id, answers=answers, questions=questions)
 
 
 @app.route("/question/<string:question_id>/edit/", methods=["POST", "GET"])
 def edit(question_id):
-    questions = data_manager.get_questions()
-    question_to_update = get_data(questions, question_id)
-    update_form = dict(request.form)
+    question_to_update = data_manager.get_data_question(question_id)
     if request.method == "POST":
-        for row in questions:
-            if row["id"] == question_to_update["id"]:
-                question_to_update["title"] = update_form["title"]
-                question_to_update["message"] = update_form["message"]
-                questions.pop(questions.index(row))
-                questions.append(question_to_update)
-                with open("/home/gergely/projects/2.module/ask-mate-1-python-Andrassyattila/sample_data/question.csv", 'w', newline='') as csvfile:
-                    fieldnames = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
-                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                    writer.writeheader()
-                    for row in questions:
-                        writer.writerow(row)
+        new_title = request.form["update-title"]
+        new_message = request.form["update-message"]
+        data_manager.update_question(new_title, new_message, question_id)
         return redirect("/question/"+question_id)
     else:
         
-        return render_template("edit.html", question_id=question_id, questions = questions, question_to_update = question_to_update,update_form=update_form )
+        return render_template("edit.html", question_id=question_id, question_to_update = question_to_update)
 
-    
 
 @app.route("/add_question", methods=["POST", "GET"])
 def question():
     if request.method == "POST":
-        data = dict(request.form)
-        with open("/home/gergely/projects/2.module/ask-mate-1-python-Andrassyattila/sample_data/question.csv", "a") as f:
-            f.writelines(str(get_id(data_handler.open_db_questions()))+",")
-            for i in range(2):
-                f.writelines(" " + ",")
-            f.writelines("0"+",")
-            for i in data:
-                if i == "question_name":
-                    f.writelines(data[i] + ",")
-                elif i == "description":
-                    f.writelines(data[i])
-            f.writelines("," + " " + "\n")
+        data_manager.add_new_question(0, 0, 0, request.form["question_name"], request.form["description"], "")
         return redirect("/list")
     return render_template("add-question.html")
 
@@ -94,20 +46,8 @@ def question():
 @app.route("/question/<string:question_id>/newanswer/", methods=["POST", "GET"])
 def answer(question_id):
     if request.method == "POST":
-        data = dict(request.form)
-        with open("/home/gergely/projects/2.module/ask-mate-1-python-Andrassyattila/sample_data/answer.csv", "a") as f:
-            f.writelines(str(get_id_ans(data_handler.open_db_answer()))+",")
-            for i in range(3):
-                if i == 2:
-                    f.writelines(question_id + ",")
-                elif i == 1:
-                    f.writelines("0"+",")
-                else:
-                    f.writelines(" " + ",")
-            
-            f.writelines(data["comment"])
-            f.writelines("," + " " + "\n")
-        return redirect("/list")
+        data_manager.add_new_answer(0, 0, question_id, request.form["comment"],"")
+        return redirect("/question/"+question_id)
     return render_template("newanswer.html")
 
 
@@ -115,20 +55,11 @@ def answer(question_id):
 @app.route("/<string:question_id>/vote_up", methods=["POST","GET"])
 def vote_up(question_id):
     question_id=question_id
-    questions = data_manager.get_questions()
-    question_to_update = get_data(questions, question_id)
+    question_to_update = data_manager.get_data_question(question_id)
     vote = int(question_to_update["vote_number"])
     if request.method == "GET":
         vote += 1
-        question_to_update["vote_number"]=str(vote)
-        questions.pop(questions.index(question_to_update))
-        questions.append(question_to_update)
-        with open("/home/gergely/projects/2.module/ask-mate-1-python-Andrassyattila/sample_data/question.csv", 'w', newline='') as csvfile:
-            fieldnames = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            for row in questions:
-                writer.writerow(row)
+        data_manager.update_question_vote(vote, question_id)
         return redirect("/list")
    
 
